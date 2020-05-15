d14a22868a26fdb91cda0dd2a5f5d055d222f71f
Kate <kbaldwin702@gmail.com>
2020-05-15 07:01:00 -0700

    Added resource view

--- pythonclubproject/pythonclubapp/views.py
+++ pythonclubproject/pythonclubapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Meeting, MeetingMinutes, Resource, Event
 
 # Create your views here.
 def index (request):
     return render(request, 'pythonclubapp/index.html')
+
+#View that returns all the resources
+def getResources (request):
+    resource_list = Resource.objects.all()
+    return render(request, 'pythonclubapp/resource.html', {'resource_list' : resource_list})
