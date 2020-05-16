74d70cfa97f8d9696b66f66e7619b147193013e3
Kate <kbaldwin702@gmail.com>
2020-05-15 22:58:46 -0700

    Added meeting and detail views

--- pythonclubproject/pythonclubapp/urls.py
+++ pythonclubproject/pythonclubapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('getResources/', views.getResources, name='resources'),
+    path('getMeetings/', views.getMeetings, name='meetings'),
+    path('getMeetingDetails/<int:id>', views.meetingDetails, name='details'),
 ]
--- pythonclubproject/pythonclubapp/views.py
+++ pythonclubproject/pythonclubapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Meeting, MeetingMinutes, Resource, Event
 
 # Create your views here.
@@ -9,3 +9,19 @@ def index (request):
 def getResources (request):
     resource_list = Resource.objects.all()
     return render(request, 'pythonclubapp/resource.html', {'resource_list' : resource_list})
+
+#View that returns all the meetings
+def getMeetings (request):
+    meeting_list = Meeting.objects.all()
+    return render(request, 'pythonclubapp/meeting.html', {'meeting_list' : meeting_list})
+
+#This view takes parameter int ID and retrieves the meeting with that ID.
+def meetingDetails(request, id):
+    details=get_object_or_404(Meeting, pk=id)
+    #reviews=Review.objects.filter(product=id).count()
+    context={
+        'meeting' : details,
+        #'discount' : discount,
+        #'reviews' : reviews,
+    }
+    return render(request, 'pythonclubapp/details.html', context=context)
